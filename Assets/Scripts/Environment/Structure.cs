50b778cbd2dd10c81e34d1e1bee0cc01b11342f3
Nikolay Hadzhiev <grochobg@gmail.com>
2025-05-08 23:18:13 +0200

    update structure

--- Assets/Scripts/Environment/Structure.cs
+++ Assets/Scripts/Environment/Structure.cs
@@ -8,12 +8,28 @@ namespace TDTest.Structural
     /// </summary>
     public class Structure : MonoBehaviour
     {
-        [field: SerializeField] public Transform gridHolder { get; private set; }
+        [field: SerializeField] public Transform GridHolder { get; private set; }
         [field: SerializeField] public GridDescription GridDescription { get; private set; }
+
+        public Vector3 WorldStartPos { get; private set; }
 
         void Start()
         {
             Statics.Grid.RegisterStructure(this);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+
+            var centerPos = GridHolder.position;
+
+            // planes are 10x10, this is lower left before scale
+            var localLowerLeft = new Vector3(-5f, 0f, -5f);
+            var worldStartPos = Vector3.Scale(localLowerLeft, GridHolder.localScale);
+
+            WorldStartPos = worldStartPos;
+            Gizmos.DrawSphere(worldStartPos, 1f);
         }
     }
 }
