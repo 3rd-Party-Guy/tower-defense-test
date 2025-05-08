5f5b866693d4887bceec6ec2b4f09e1361faeb53
Nikolay Hadzhiev <grochobg@gmail.com>
2025-05-08 23:17:47 +0200

    implement get world position

--- Assets/Scripts/Systems/Implementations/Grid.cs
+++ Assets/Scripts/Systems/Implementations/Grid.cs
@@ -6,19 +6,37 @@ namespace TDTest.Structural
     {
         Cell[,] cells;
         GridDescription description;
+        Vector3 worldOrigin;
 
-        public Grid(GridDescription description)
+        public Grid(Structure structure)
         {
-            this.description = description;
+            this.description = structure.GridDescription;
             cells = new Cell[description.Width, description.Height];
+
+            CalculateWorldOrigin(structure);
         }
 
-        public void ToWorldPos(int x, int y)
+        public Vector3 ToWorldPos(int x, int y)
         {
             Debug.Assert(x >= 0 && x < description.Width, $"Grid: Tried to get world position for invalid x Coordinate ({x})");
             Debug.Assert(y >= 0 && y < description.Height, $"Grid: Tried to get world position for invalid y Coordinate ({y})");
 
+            var cellSize = description.CellSize;
 
+            return new Vector3(
+                worldOrigin.x + cellSize * x,
+                worldOrigin.y,
+                worldOrigin.z + cellSize * y);
+        }
+
+        void CalculateWorldOrigin(Structure structure)
+        {
+            var gridHolder = structure.GridHolder;
+            var centerPos = gridHolder.position;
+            var localLowerLeft = new Vector3(-5f, 0f, -5f);
+            var worldStartPos = Vector3.Scale(localLowerLeft, gridHolder.localScale);
+
+            worldOrigin = worldStartPos;
         }
     }
 }
--- Assets/Scripts/Systems/Implementations/GridSystem.cs
+++ Assets/Scripts/Systems/Implementations/GridSystem.cs
@@ -5,31 +5,36 @@ namespace TDTest.Structural
 {
     public class GridSystem : ISystem
     {
-        List<Structure> structures;
+        Dictionary<Structure, Grid> structureGridLookup;
 
         public void Initialize()
         {
-            structures = new();
+            structureGridLookup = new();
         }
 
         public void Tick(float deltaTime, float unscaledDeltaTime) { }
 
         public void Deinitialize()
         {
-            structures = null;
+            structureGridLookup = null;
         }
 
         public void RegisterStructure(Structure newStructure)
         {
-            Debug.Assert(!structures.Contains(newStructure), "GridSystem: Tried to register structure twice.");
-
-            structures.Add(newStructure);
+            Debug.Assert(!structureGridLookup.ContainsKey(newStructure), "GridSystem: Tried to register structure twice.");
             CreateGridForStructure(newStructure);
+        }
+
+        public Vector3 GridToWorldPos(Structure structure, int x, int y)
+        {
+            Debug.Assert(structureGridLookup[structure] != null, "GridSystem: Tried to calculate world position for unregistered grid.");
+            return structureGridLookup[structure].ToWorldPos(x, y);
         }
 
         void CreateGridForStructure(Structure structure)
         {
             Debug.Assert(structure.GridDescription.IsValid(out var err), err);
+            structureGridLookup.Add(structure, new Grid(structure));
         }
     }
 }
